12276d2b99713f3fa908120f1210f99bbd4fb402
Iago Mauricio <iagomauricio7@gmail.com>
2023-10-05 20:30:27 -0300

    versão pronta google authenticator

--- auth.py
+++ auth.py
@@ -1,14 +1,30 @@
+import os
 import pyotp
 import qrcode
 
-key = pyotp.random_base32()
+def authenticator():
+    # Nome do arquivo QR Code
+    qrcode_filename = "qrcode.png"
 
-url = pyotp.totp.TOTP(key).provisioning_uri(name='Iago', issuer_name='Teste')
+    # Verifica se o arquivo já existe e exclui se necessário
+    if os.path.exists(qrcode_filename):
+        os.remove(qrcode_filename)
 
-qrcode.make(url).save("qrcode.png")
+    # Gera uma nova chave
+    key = pyotp.random_base32()
 
-totp = pyotp.TOTP(key)
+    # Gera a URL para o TOTP
+    url = pyotp.totp.TOTP(key).provisioning_uri(name='admin')
 
-while True:
-    print(totp.verify(input("Code: ")))
+    # Cria e salva o QR Code
+    qrcode.make(url).save(qrcode_filename)
+    # Inicializa o TOTP com a chave
+    global totp
+    totp = pyotp.TOTP(key)
+
+
+    # Loop para verificação contínua
+    while True:
+        print(totp.verify(input("Code: ")))
+
 
